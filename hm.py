1e7cabafdd10bd0ecfaf7855059bb53178fc7c72
byteMaziz <tahir.muhammad@estudiantat.upc.edu>
2024-06-07 05:06:38 +0200

    gestion de tipos inicial

--- hm.py
+++ hm.py
@@ -7,16 +7,16 @@ from hmParser import hmParser
 from hmVisitor import hmVisitor
 
 import streamlit as st
-
+import pandas as pd
 
 ######################################################################################
-################################ DEFINICIO DE L'ARBRE ################################
+################################ DEFINICION DEL ARBOL ################################
 ######################################################################################
 
 @dataclass
 class Node:
-    id: int         # identificador unic del node
-    symb: str       # simbol que representa el node
+    id: int         # identificador unico del nodo
+    symb: str       # simbolo que representa el nodo
     left: Tree
     right: Tree
 
@@ -25,14 +25,18 @@ class Void:
         
 Tree = Node | Void
 
-# ---------------------------------- FUNCIONS D'ARBRE ----------------------------------
+#-------------------------------- FUNCIONES DEL ARBOL --------------------------------
 
 def fromTreeToDotGraph(tree: Tree) -> str:
+    # recorre el arbol de la forma adecuada para crear el grafo
     def traverse(node: Tree, dot_lines: list):
         if isinstance(node, Void):
-            return        
+            return
         if isinstance(node, Node):
+            # mediante el identificador unico, definimos el texto de los vertices
             dot_lines.append(f'    {node.id} [label="{node.symb}"];')
+            
+            # se crean las aristas en orden
             if isinstance(node.left, Node):
                 dot_lines.append(f'    {node.id} -- {node.left.id};')
                 traverse(node.left, dot_lines)
@@ -40,6 +44,7 @@ def fromTreeToDotGraph(tree: Tree) -> str:
                 dot_lines.append(f'    {node.id} -- {node.right.id};')
                 traverse(node.right, dot_lines)
 
+    # se escribe la sintaxis en DOT para crear el grafo mediante el recorrido
     dot_lines = ["strict graph {"]
     traverse(tree, dot_lines)
     dot_lines.append("}")
@@ -47,14 +52,15 @@ def fromTreeToDotGraph(tree: Tree) -> str:
 
 
 ######################################################################################
-############################## DEFINICIO DEL VISITADOR ###############################
+############################## DEFINICION DEL VISITADOR ##############################
 ######################################################################################
 
 class TreeVisitor(hmVisitor):
-    def __init__(self):
-        self.current_id = 0
+    def __init__(self, symbol_table):
+        self.current_id = 0                 # identificador unico que se da a cada uno
+        self.symbol_table = symbol_table    # tabla de simbolos hasta ahora
 
-    # Retorna un identificador unic
+    # Retorna un identificador nuevo unico
     def next_id(self):
         self.current_id += 1
         return self.current_id
@@ -62,7 +68,14 @@ class TreeVisitor(hmVisitor):
     # root : expr
     def visitRoot(self, ctx:hmParser.RootContext):
         return self.visit(ctx.expr())
-
+	
+    # typeDefinition : term '::' type
+    def visitTypeDefinition(self, ctx: hmParser.TypeDefinitionContext):
+        term = ctx.term().getText()
+        type_expr = ctx.type_().getText()
+        self.symbol_table[term] = type_expr
+        return Void()
+    
     # abstraction : '\\' VARIABLE '->' expr;
     def visitAbstraction(self, ctx:hmParser.AbstractionContext):
         variable = ctx.VARIABLE().getText()
@@ -81,7 +94,8 @@ class TreeVisitor(hmVisitor):
         left = self.visit(ctx.term(0))
         right = self.visit(ctx.term(1))
         return Node(self.next_id(), '@', left, right)
-    
+
+    # term: '(' expr ')'                # ParenExpr
     def visitParenExpr(self, ctx:hmParser.ParenExprContext):
         return self.visit(ctx.expr())
 
@@ -105,13 +119,12 @@ class TreeVisitor(hmVisitor):
 ######################################## MAIN ########################################
 ######################################################################################
 
-
-
 st.write("""
-         # L'analitzador de tipus HinNer
-         Projecte de llenguatjes de programació - Q2 2023-24
+         # El analizador de tipos HinNer
+         #### Proyecto de lenguajes de programación - Q2 2023/24
          ***
-         Codigos de prueba para tener a mano:  
+         Códigos de prueba para tener a mano:  
+         ```
          2  
          x  
          (+) 2  
@@ -119,12 +132,13 @@ st.write("""
          (\\x -> (+) 2 x) 4  
          ((\\x -> (+) 2 x) ((\\y -> (+) 3 y) 6))  
          2 :: N  
-         (+) :: N -> N -> N  
+         (+) :: N -> N -> N
+         ```  
          """)
 
-stInput = st.text_input("Expressió", 
-                        value=("Hola"),
-                        help="Introdueix la teva expressió i prem *Enter* per a compilar-la"
+stInput = st.text_input("Entrada", 
+                        value="Ejemplo",
+                        help="Introduce la expresión y presiona *Enter* en tu teclado para compilarla"
                         )
 
 input_stream = InputStream(stInput)
@@ -133,13 +147,26 @@ token_stream = CommonTokenStream(lexer)
 parser = hmParser(token_stream)
 tree = parser.root()
 
+# Para no perder la tabla de simbolos cada vez que haya rerun de streamlit se usa session_state
+if 'symbol_table' not in st.session_state:
+    st.session_state['symbol_table'] = {}
+
 if parser.getNumberOfSyntaxErrors() != 0:
     st.error(f"{parser.getNumberOfSyntaxErrors()} error(s) de sintaxi.", icon="🚨")
 else:
-    visitor = TreeVisitor()
+    visitor = TreeVisitor(st.session_state['symbol_table'])
     result_tree = visitor.visit(tree)
     
-    graphviz_code = fromTreeToDotGraph(result_tree)
-    st.graphviz_chart(graphviz_code)  
+    st.session_state['symbol_table'] = visitor.symbol_table
+    st.write("#### Contenido de la tabla de símbolos")
+    symbol_table_data = [{"Símbolo": k, "Tipo": v} for k, v in st.session_state['symbol_table'].items()]
+    symbol_table_df = pd.DataFrame(symbol_table_data)
+    st.table(symbol_table_df)
+    
+    # En caso que haya sido una definicion de tipo (donde el visitador retorna Void()), no se imprime el arbol
+    if isinstance(result_tree, Node):
+        graphviz_code = fromTreeToDotGraph(result_tree)
+        st.graphviz_chart(graphviz_code)
+
 
 st.write(tree.toStringTree(recog=parser))
